9e382bbfe362049ae97e75e72876d546c614ec71
Chavdar Momchilov <chavmom@gmail.com>
2024-06-19 11:41:32 +0300

    Changes to be committed: 	new file:   classes/Problem07.py

---
+++ classes/Problem07.py
@@ -0,0 +1,10 @@
+class Dog:
+    def __str__(self):
+        return "This is a dog class."
+    
+sam = Dog()
+
+print(sam) # will print the return of the string magic method
+# this is a dog class.
+
+
