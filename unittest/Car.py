9f2453c1774f60b5c3f53eb206b36177d9f3c801
Chavdar Momchilov <chavmom@gmail.com>
2024-06-19 08:53:37 +0300

    Changes to be committed: 	new file:   unittest/Car.py 	modified:   unittest/Problem_test.py 	new file:   unittest/__pycache__/Car.cpython-39.pyc

---
+++ unittest/Car.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self,
+                 model="",
+                 color="",
+                 year=""):
+        self._model = model
+        self._color = color
+        self._year = year
+    
+
+
+
+
--- unittest/Problem_test.py
+++ unittest/Problem_test.py
@@ -1,4 +1,5 @@
 import unittest
+from Car import Car
 
 def sum(a, b):
     return a + b
@@ -6,11 +7,18 @@ def sum(a, b):
 def sub(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_init_default_values(self):
+        car = Car()
         self.assertEqual(sum(2,2), 4)
         self.assertEqual(sub(4, 3), 1)
         self.assertEqual(sub(4, 2), 2)
+        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(car._model, "")
 
 
 if __name__ == '__main__':
