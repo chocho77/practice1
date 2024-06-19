c1b96684618870f500fbf7ca00e4d27fcb1a814e
Chavdar Momchilov <chavmom@gmail.com>
2024-06-19 12:57:59 +0300

    Changes to be committed: 	modified:   classes/Problem08.py

--- classes/Problem08.py
+++ classes/Problem08.py
@@ -18,10 +18,31 @@ class Student:
         self._email = email
         self._tel = tel
         Student.number_of_students += 1
+    
+    def print_info(self):
+        print(
+            self._first_name,
+            self._middle_name,
+            self._last_name,
+            self._email,
+            self._university
+        )
+    
+    def __str__(self):
+        return f"{self._first_name} {self._middle_name} {self._last_name} {self._email} {self._university}"
+
 
     @property
     def year(self):
         return self._year
+    
+    @property
+    def tel(self):
+        return self._tel
+    
+    @tel.setter
+    def tel(self, value):
+        self._tel = value
     
     
 #john = Student()
@@ -30,11 +51,16 @@ class Student:
 #print(type(john._first_name))
 #print(john._first_name)
 
-john = Student()
+john = Student("John", "Eduard", "Smith", 1977, 'programer', \
+               'TU-Varna', 'chavmom@gmail.com', '0889234567')
 print(Student.number_of_students)
 smith = Student()
 print(Student.number_of_students)
-
+john.tel = "0882345678"
+print(john.tel)
+john.print_info()
+print("----------")
+print(john)
 
 
 
