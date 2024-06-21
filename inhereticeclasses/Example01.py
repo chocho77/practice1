288d575c8ddb0a7d5550dbaef85e2a0ca4832795
Chavdar Momchilov <chavmom@gmail.com>
2024-06-21 18:33:21 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   inhereticeclasses/Example01.py

--- inhereticeclasses/Example01.py
+++ inhereticeclasses/Example01.py
@@ -1,15 +1,33 @@
 class Animal:
+    def __init__(self, name) -> None:
+        self._name = name
+
     def makeSound(self):
         print("Roar")
 
 class Dog(Animal):    # inherites class Animal
-    pass
+    def __init__(self, name):
+        super().__init__(name)
+
+    @property
+    def name(self):
+        return self._name
+
 
 class Cat(Animal):
-    pass
+    def __init__(self, name):
+        super().__init__(name)
+    
+    @property
+    def name(self):
+        return self._name
 
 
-sam = Dog()
-Cat().makeSound()
+sam = Dog("Sam")
+cat = Cat("Tom")
+print(sam.name)
+print(cat.name)
+
 sam.makeSound()
+cat.makeSound()
 
