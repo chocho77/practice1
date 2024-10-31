31771e7f08ef0459c18983ecee4af097e3f0f0eb
Chavdar Momchilov <chavmom@gmail.com>
2024-10-31 09:54:55 +0200

    continue example operations

--- lecture02/arithmetic_operations.py
+++ lecture02/arithmetic_operations.py
@@ -5,3 +5,8 @@ print(2/3)
 print(2//4)
 print(4//2)
 print(5//2)
+print(4/3) # Float division
+print(4//3) # Integer division
+print(5%3) # Remainder
+print(2 ** 3) # Power
+print((4 + 5) * 2)   # Change priority of operations
