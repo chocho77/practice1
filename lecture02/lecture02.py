cb95f8906def85d46f2dc0790dcb8b960e509886
Chavdar Momchilov <chavmom@gmail.com>
2024-10-31 03:55:34 +0200

    additional comment line

--- lecture02/lecture02.py
+++ lecture02/lecture02.py
@@ -2,4 +2,4 @@ x = 1
 # this is a comment
 # The initial value of x is 1.
 if x > 0:
-    print("These three lines are Physical/Logical lines.")
+    print("These three lines are Physical/Logical lines.") # print string
