c4013dd99d2497a6abc1a9dfd488a19261accc30
Chavdar Momchilov <chavmom@gmail.com>
2024-10-31 09:49:55 +0200

    example program

---
+++ lecture02/variables_example.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+first_name = "John" # initialization a string variable
+age = 23 # initialization number
+is_male = True # bool
+is_married = True # bool snake case
+firstName = "John" # Camel Case
+FirstName = "John" # Pascal Case
+
+x = 1 # Integer
+print(f"x = {x}")
+print(f"type of x : {type(x)}")
+y = 3.4 # Float
+print(f"y = {y}")
+print(f"type of y : {type(y)}")
+pi = 3.141592 # Real number or Float
+overflow = 3.12345678910111213
+print(overflow)
+limit_float_number = 3.123456789101112
+print(limit_float_number)
+is_user_logged_in = False # Boolean
+print(is_user_logged_in)
+is_user_logged_in = True
+print(is_user_logged_in)
+
+comment = "Hello I am a string." #str
+print(comment)
+
+current_datetime = datetime.now()
+print(current_datetime)
+current_datetime = datetime.now()
+print(current_datetime)
+print(1, 2, 3) # print multiple values
+print(1, 2, 3, sep="-") # use separator
+print(1, 2, 3, sep="!") # use separator with different symbol
+print(1, 2, 3, end="!!!!\n") # use parameter end (with parameter end)
+
+# Print with both
+print(1, 2, 3, sep="-", end="!!!\n")
+
+print(2 * "*")
+
+
+
+
+
+
