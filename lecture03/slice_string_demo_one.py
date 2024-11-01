629358e0457e2e6cedc8bb5c8abb9ed51cee5411
Chavdar Momchilov <chavmom@gmail.com>
2024-11-01 15:45:01 +0200

    continue with slicing strings examples

--- lecture03/slice_string_demo_one.py
+++ lecture03/slice_string_demo_one.py
@@ -16,8 +16,21 @@ def reverse_string_demo_one() -> None:
     print(name)
     print(name[-1::-1])
 
+def slice_string_demo_two() -> None:
+    names = "Momchil Ivan Drago"
+    print(names)
+    print(names[:-6])
+    print(names[7:-6])
+    names_one = f"{names[:-6]} {names[7:-6]}"
+    print(names_one)
+    names_two = names[:-6] + names[7:-6]
+    print(names_two)
+    pass
+
 
 if __name__ == '__main__':
     slice_string_demo_one()
     reverse_string_demo_one()
+    slice_string_demo_two()
+
 
