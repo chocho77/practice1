e1975bfd10bf432b73a36f149433e4bef10694a5
Chavdar Momchilov <chavmom@gmail.com>
2024-11-02 10:40:08 +0200

    exchange values of variables program improvements

--- lecture02/problems/Problem09.py
+++ lecture02/problems/Problem09.py
@@ -1,9 +1,11 @@
-def exchange_values() -> None:
-    x, y = 5, 10
+def exchange_values(num1: int, num2: int) -> None:
+    x, y = num1, num2
     print(f"x = {x}, y = {y}")
     x, y = y, x
     print(f"x = {x}, y = {y}")
 
 
 if __name__ == '__main__':
-    exchange_values()
+    exchange_values(5,6)
+    exchange_values(6,7)
+    exchange_values(56,78)
