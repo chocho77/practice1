d3b699bb1780cde581ebbbb95a2b57a7f3da33aa
Chavdar Momchilov <chavmom@gmail.com>
2024-11-03 19:34:18 +0200

    more exercises

---
+++ lecture03/problems/Problem08.py
@@ -0,0 +1,28 @@
+def solution() -> None:
+    school = "Vasil Drumev"
+    print(school)
+
+def solution_one() -> None:
+    country = "usa"
+    correct_country = country.upper()
+    print(correct_country)
+
+def solution_two() -> None:
+    country = "USA"
+    correct_country = country.lower()
+    print(correct_country)
+
+def solution_three() -> None:
+    filename = "hello.py"
+    print(filename.endswith(".java"))
+    print(filename.index("py"))
+    print(filename.startswith("word"))
+
+
+if __name__ == '__main__':
+    solution()
+    solution_one()
+    solution_two()
+    solution_three()
+
+
