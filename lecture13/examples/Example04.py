d79668a86cf8d662d628ee3be28c2a82a545831b
Chavdar Momchilov <chavmom@gmail>
2024-11-26 19:00:21 +0200

    continue with learning

--- lecture13/examples/Example04.py
+++ lecture13/examples/Example04.py
@@ -1,5 +1,8 @@
 from math import sqrt as square_root # use alias
 import test   # add functionality from another file
+import test_one as test_lib
+from test_one import print_info
+
 print(square_root(4.0))
 
 x = 45.5
@@ -7,3 +10,7 @@ x = 45.5
 print(square_root(x))
 print(test.HELLO_WORLD)
 print(test.sol_one())
+hello_from_test_one = test_lib.hello()
+print(hello_from_test_one)
+test_lib.print_info("Chavdar", 46)
+print_info("Koko", 30)
---
+++ lecture13/examples/test_one.py
@@ -0,0 +1,16 @@
+# create our own module in a text editor
+
+# variables to import later
+
+length = 5
+width = 10
+
+# functions to import later
+
+def print_info(name, age):
+    print(f"{name} is {age} years old.")
+
+
+def hello() -> str:
+    return "Hello, from test_one."
+
